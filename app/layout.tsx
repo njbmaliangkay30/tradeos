e26cdf6776f8711eb308b1b6f4ce8f9cba7fc4be
njbmaliangkay30 <njbmaliangkay30@gmail.com>
2026-05-06 15:26:00 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      {/* UPDATE WARNA BACKGROUND DI SINI */}
-      <body className={`${inter.className} bg-[#09090b] text-zinc-300 min-h-screen flex`}>
+      <body className={`${inter.className} bg-[#09090b] text-zinc-300 min-h-screen flex relative overflow-x-hidden`}>
+        
+        {/* POLA BACKGROUND GRID TIPIS (Opacity 20%) */}
+        <div className="fixed inset-0 z-[-1] h-full w-full bg-[#09090b] bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"></div>
+        {/* EFEK CAHAYA GLOWING DI TENGAH ATAS */}
+        <div className="fixed left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-cyan-500 opacity-[0.03] blur-[100px]"></div>
+
         <Sidebar />
         <main className="pl-20 w-full min-h-screen selection:bg-cyan-500/30">
           <div className="p-8 max-w-7xl mx-auto">
